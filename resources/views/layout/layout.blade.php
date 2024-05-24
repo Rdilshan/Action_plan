27cc5f51684cc15ed02d0868e254bbf793cf8ef5
Randika Dilshan <rdilshan077788@gmail.com>
2024-05-24 10:14:48 +0530

    layout craete

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Project</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="#">
+    <meta name="keywords"
+        content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="#">
+    <!-- Favicon icon -->
+
+    <link rel="icon" href="{{url('assets\images\favicon.ico')}}" type="image/x-icon">
+
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{url('bower_components\bootstrap\css\bootstrap.min.css')}}">
+    <!-- feather Awesome -->
+    <link rel="stylesheet" type="text/css" href="{{url('assets\icon\feather\css\feather.css')}}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{url('assets\css\style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets\css\jquery.mCustomScrollbar.css')}}">
+</head>
+
+<body>
+    <!-- Pre-loader start -->
+    <div class="theme-loader">
+        <div class="ball-scale">
+            <div class='contain'>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Pre-loader end -->
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
+
+            <nav class="navbar header-navbar pcoded-header">
+                <div class="navbar-wrapper">
+
+                    <div class="navbar-logo">
+                        <a class="mobile-menu" id="mobile-collapse" href="#!">
+                            <i class="feather icon-menu"></i>
+                        </a>
+                        <a href="index-1.htm">
+                            <!-- <img class="img-fluid" src="..\files\assets\images\logo.png" alt="Theme-Logo"> -->
+                        </a>
+                        <a class="mobile-options">
+                            <i class="feather icon-more-horizontal"></i>
+                        </a>
+                    </div>
+
+                    <div class="navbar-container container-fluid">
+
+                        <ul class="nav-right">
+
+                            <li class="user-profile header-notification">
+                                <div class="dropdown-primary dropdown">
+                                    <div class="dropdown-toggle" data-toggle="dropdown">
+                                        <img src="{{url('assets\images\avatar-4.jpg')}}" class="img-radius"
+                                            alt="User-Profile-Image">
+                                        <span>John Doe</span>
+                                        <i class="feather icon-chevron-down"></i>
+                                    </div>
+                                    <ul class="show-notification profile-notification dropdown-menu"
+                                        data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                                        <li>
+                                            <a href="#!">
+                                                <i class="feather icon-settings"></i> Settings
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="user-profile.htm">
+                                                <i class="feather icon-user"></i> Profile
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="email-inbox.htm">
+                                                <i class="feather icon-mail"></i> My Messages
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="auth-lock-screen.htm">
+                                                <i class="feather icon-lock"></i> Lock Screen
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="auth-normal-sign-in.htm">
+                                                <i class="feather icon-log-out"></i> Logout
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+            <!-- Sidebar inner chat end-->
+            <div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+                    <nav class="pcoded-navbar">
+                        <div class="pcoded-inner-navbar main-menu">
+                            <div class="pcoded-navigatio-lavel">Navigation</div>
+                            <ul class="pcoded-item pcoded-left-item">
+
+                                <li class="active">
+                                    <a href="index.htm">
+                                        <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                                        <span class="pcoded-mtext">Dashboard</span>
+                                    </a>
+                                </li>
+
+
+
+                                <li class="pcoded-hasmenu">
+                                    <a href="javascript:void(0)">
+                                        <span class="pcoded-micon"><i class="feather icon-sidebar"></i></span>
+                                        <span class="pcoded-mtext">Page layouts</span>
+
+                                    </a>
+                                    <ul class="pcoded-submenu">
+                                        <li class=" pcoded-hasmenu">
+                                            <a href="javascript:void(0)">
+                                                <span class="pcoded-mtext">Vertical</span>
+                                            </a>
+                                            <ul class="pcoded-submenu">
+                                                <li class=" ">
+                                                    <a href="menu-static.htm">
+                                                        <span class="pcoded-mtext">Static Layout</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-header-fixed.htm">
+                                                        <span class="pcoded-mtext">Header Fixed</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-compact.htm">
+                                                        <span class="pcoded-mtext">Compact</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-sidebar.htm">
+                                                        <span class="pcoded-mtext">Sidebar Fixed</span>
+                                                    </a>
+                                                </li>
+
+                                            </ul>
+                                        </li>
+                                        <li class=" pcoded-hasmenu">
+                                            <a href="javascript:void(0)">
+                                                <span class="pcoded-mtext">Horizontal</span>
+                                            </a>
+                                            <ul class="pcoded-submenu">
+                                                <li class=" ">
+                                                    <a href="menu-horizontal-static.htm" target="_blank">
+                                                        <span class="pcoded-mtext">Static Layout</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-horizontal-fixed.htm" target="_blank">
+                                                        <span class="pcoded-mtext">Fixed layout</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-horizontal-icon.htm" target="_blank">
+                                                        <span class="pcoded-mtext">Static With Icon</span>
+                                                    </a>
+                                                </li>
+                                                <li class=" ">
+                                                    <a href="menu-horizontal-icon-fixed.htm" target="_blank">
+                                                        <span class="pcoded-mtext">Fixed With Icon</span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class=" ">
+                                            <a href="menu-bottom.htm">
+                                                <span class="pcoded-mtext">Bottom Menu</span>
+                                            </a>
+                                        </li>
+                                        <li class=" ">
+                                            <a href="box-layout.htm" target="_blank">
+                                                <span class="pcoded-mtext">Box Layout</span>
+                                            </a>
+                                        </li>
+                                        <li class=" ">
+                                            <a href="menu-rtl.htm" target="_blank">
+                                                <span class="pcoded-mtext">RTL</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="">
+                                    <a href="navbar-light.htm">
+                                        <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
+                                        <span class="pcoded-mtext">Navigation</span>
+                                    </a>
+                                </li>
+
+                            </ul>
+
+
+                        </div>
+                    </nav>
+                    @yield('contend')
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript" src="{{url('bower_components\jquery\js\jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('bower_components\jquery-ui\js\jquery-ui.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('bower_components\popper.js\js\popper.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('bower_components\bootstrap\js\bootstrap.min.js')}}"></script>
+    <!-- jquery slimscroll js -->
+    <script type="text/javascript" src="{{url('bower_components\jquery-slimscroll\js\jquery.slimscroll.js')}}"></script>
+    <!-- modernizr js -->
+    <script type="text/javascript" src="{{url('bower_components\modernizr\js\modernizr.js')}}"></script>
+
+    <!-- amchart js -->
+    <script src="{{url('assets\pages\widget\amchart\amcharts.js')}}"></script>
+    <script src="{{url('assets\pages\widget\amchart\serial.js')}}"></script>
+    <script src="{{url('assets\pages\widget\amchart\light.js')}}"></script>
+    <script src="{{url('assets\js\jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('assets\js\SmoothScroll.js')}}"></script>
+    <script src="{{url('assets\js\pcoded.min.js')}}"></script>
+    <!-- custom js -->
+    <script src="{{url('assets\js\vartical-layout.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('assets\pages\dashboard\custom-dashboard.js')}}"></script>
+    <script type="text/javascript" src="{{url('assets\js\script.min.js')}}"></script>
+
+</body>
+
+</html>
