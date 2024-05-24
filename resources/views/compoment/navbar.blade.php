9288bb0de54177635154661d9d1bd1d42b0534c3
Randika Dilshan <rdilshan077788@gmail.com>
2024-05-24 10:23:38 +0530

    navbar add

---
+++ resources/views/compoment/navbar.blade.php
@@ -0,0 +1,62 @@
+<nav class="navbar header-navbar pcoded-header">
+    <div class="navbar-wrapper">
+
+        <div class="navbar-logo">
+            <a class="mobile-menu" id="mobile-collapse" href="#!">
+                <i class="feather icon-menu"></i>
+            </a>
+            <a href="index-1.htm">
+                <!-- <img class="img-fluid" src="..\files\assets\images\logo.png" alt="Theme-Logo"> -->
+            </a>
+            <a class="mobile-options">
+                <i class="feather icon-more-horizontal"></i>
+            </a>
+        </div>
+
+        <div class="navbar-container container-fluid">
+
+            <ul class="nav-right">
+
+                <li class="user-profile header-notification">
+                    <div class="dropdown-primary dropdown">
+                        <div class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="{{url('assets\images\avatar-4.jpg')}}" class="img-radius"
+                                alt="User-Profile-Image">
+                            <span>John Doe</span>
+                            <i class="feather icon-chevron-down"></i>
+                        </div>
+                        <ul class="show-notification profile-notification dropdown-menu"
+                            data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                            <li>
+                                <a href="#!">
+                                    <i class="feather icon-settings"></i> Settings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="user-profile.htm">
+                                    <i class="feather icon-user"></i> Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="email-inbox.htm">
+                                    <i class="feather icon-mail"></i> My Messages
+                                </a>
+                            </li>
+                            <li>
+                                <a href="auth-lock-screen.htm">
+                                    <i class="feather icon-lock"></i> Lock Screen
+                                </a>
+                            </li>
+                            <li>
+                                <a href="auth-normal-sign-in.htm">
+                                    <i class="feather icon-log-out"></i> Logout
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
