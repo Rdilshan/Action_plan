6004d669acd052e16389edc5093e5e4caf71c54b
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-04 12:03:15 +0530

    demo admin seed

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class UsersTableSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('123'),
+                'role' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],]);
+    }
+}
