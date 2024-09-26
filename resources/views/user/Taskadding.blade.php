81fddacb1c9c78f7361bf29f8a1cc494fa05be51
ravindu <ravindulakshitha250@gmail.com>
2024-09-26 16:12:36 +0530

    Update structure

--- resources/views/user/Taskadding.blade.php
+++ resources/views/user/Taskadding.blade.php
@@ -1620,98 +1620,27 @@
                             <h4 class="sub-title">Basic Inputs</h4>
                             <form>
                               <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Simple Input</label>
+                                <label class="col-sm-2 col-form-label">Title</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control">
+                                  <input type="text" class="form-control" placeholder="Enter the title">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Placeholder</label>
+                                <label class="col-sm-2 col-form-label">Name</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control" placeholder="Type your title in Placeholder">
+                                  <input type="text" class="form-control" placeholder="Type your name">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Password</label>
+                                <label class="col-sm-2 col-form-label">Action plan Start Date</label>
                                 <div class="col-sm-10">
-                                  <input type="password" class="form-control" placeholder="Password input">
+                                  <input type="text" class="form-control" placeholder="Start date">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Read only</label>
+                                <label class="col-sm-2 col-form-label">Action plan End Date</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control" placeholder="You can't change me" readonly="">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Disable Input</label>
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control" placeholder="Disabled text" disabled="">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Predefine
-                                  Input</label>
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control" value="Enter yout content after me">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Select Box</label>
-                                <div class="col-sm-10">
-                                  <select name="select" class="form-control">
-                                    <option value="opt1">Select One Value Only</option>
-                                    <option value="opt2">Type 2</option>
-                                    <option value="opt3">Type 3</option>
-                                    <option value="opt4">Type 4</option>
-                                    <option value="opt5">Type 5</option>
-                                    <option value="opt6">Type 6</option>
-                                    <option value="opt7">Type 7</option>
-                                    <option value="opt8">Type 8</option>
-                                  </select>
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Round Input</label>
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control form-control-round"
-                                    placeholder=".form-control-round">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Maximum
-                                  Length</label>
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control" placeholder="Content must be in 6 characters"
-                                    maxlength="6">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Disable
-                                  Autocomplete</label>
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control" placeholder="Autocomplete Off"
-                                    autocomplete="off">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Static Text</label>
-                                <div class="col-sm-10">
-                                  <div class="form-control-static">Hello !... This is
-                                    static text
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Color</label>
-                                <div class="col-sm-10">
-                                  <input type="color" class="form-control">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Upload File</label>
-                                <div class="col-sm-10">
-                                  <input type="file" class="form-control">
+                                  <input type="text" class="form-control" placeholder="End date">
                                 </div>
                               </div>
                               <div class="form-group row">
@@ -1721,13 +1650,20 @@
                                     placeholder="Default textarea"></textarea>
                                 </div>
                               </div>
+                              <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Upload File</label>
+                                <div class="col-sm-10">
+                                  <input type="file" class="form-control">
+                                </div>
+                              </div>
                             </form>
 
 
                           </div>
                         </div>
 
-
+                        <div class="dt-buttons"><a class="dt-button" tabindex="0" aria-controls="multibtn-grup"
+                            href="#"><span>Submit</span></a></div>
 
 
 
