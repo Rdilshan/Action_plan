7a4c7b3d5fa63a7b8f3335e85d3ebcd23a2fe8e3
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-06 06:19:18 +0530

    simple Dashboard UI add

--- resources/views/user/Dashboard.blade.php
+++ resources/views/user/Dashboard.blade.php
@@ -6,165 +6,88 @@
     <div class="pcoded-inner-content">
         <div class="main-body">
             <div class="page-wrapper">
-                <!-- Page-header start -->
-                <div class="page-header">
-                    <div class="row align-items-end">
-                        <div class="col-lg-8">
-                            <div class="page-header-title">
-                                <div class="d-inline">
-                                    <h4>Basic Form Inputs</h4>
 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="page-header-breadcrumb">
-                                <ul class="breadcrumb-title">
-                                    <li class="breadcrumb-item">
-                                        <a href="index-1.htm"> <i class="feather icon-home"></i>
-                                        </a>
-                                    </li>
-                                    <li class="breadcrumb-item"><a href="#!">Form </a>
-                                    </li>
-                                    <li class="breadcrumb-item"><a href="#!">Form </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Page-header end -->
 
-                <!-- Page body start -->
                 <div class="page-body">
                     <div class="row">
                         <div class="col-sm-12">
                             <!-- Basic Form Inputs card start -->
-                            <div class="card">
+                            <div class="row">
 
-                                <div class="card-block">
-                                    <h4 class="sub-title">Basic Inputs</h4>
-                                    <form>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Simple
-                                                Input</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label
-                                                class="col-sm-2 col-form-label">Placeholder</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    placeholder="Type your title in Placeholder">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Password</label>
-                                            <div class="col-sm-10">
-                                                <input type="password" class="form-control"
-                                                    placeholder="Password input">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Read only</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    placeholder="You can't change me" readonly="">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Disable
-                                                Input</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    placeholder="Disabled text" disabled="">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Predefine
-                                                Input</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    value="Enter yout content after me">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Select
-                                                Box</label>
-                                            <div class="col-sm-10">
-                                                <select name="select" class="form-control">
-                                                    <option value="opt1">Select One Value Only
-                                                    </option>
-                                                    <option value="opt2">Type 2</option>
-                                                    <option value="opt3">Type 3</option>
-                                                    <option value="opt4">Type 4</option>
-                                                    <option value="opt5">Type 5</option>
-                                                    <option value="opt6">Type 6</option>
-                                                    <option value="opt7">Type 7</option>
-                                                    <option value="opt8">Type 8</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Round
-                                                Input</label>
-                                            <div class="col-sm-10">
-                                                <input type="text"
-                                                    class="form-control form-control-round"
-                                                    placeholder=".form-control-round">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Maximum
-                                                Length</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    placeholder="Content must be in 6 characters"
-                                                    maxlength="6">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Disable
-                                                Autocomplete</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control"
-                                                    placeholder="Autocomplete Off"
-                                                    autocomplete="off">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Static
-                                                Text</label>
-                                            <div class="col-sm-10">
-                                                <div class="form-control-static">Hello !... This is
-                                                    static text
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="card bg-c-yellow update-card">
+                                        <div class="card-block">
+                                            <div class="row align-items-end">
+                                                <div class="col-8">
+                                                    <h4 class="text-white">$30200</h4>
+                                                    <h6 class="text-white m-b-0">Number of Goals</h6>
+                                                </div>
+                                                <div class="col-4 text-right"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; inset: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
+                                                    <canvas id="update-chart-1" height="62" style="display: block; height: 50px; width: 88px;" width="110"></canvas>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Color</label>
-                                            <div class="col-sm-10">
-                                                <input type="color" class="form-control">
-                                            </div>
+                                        <div class="card-footer">
+                                            <p class="text-white m-b-0"><i class="feather icon-clock text-white f-14 m-r-10"></i>update : 2:15 am</p>
                                         </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Upload
-                                                File</label>
-                                            <div class="col-sm-10">
-                                                <input type="file" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Textarea</label>
-                                            <div class="col-sm-10">
-                                                <textarea rows="5" cols="5" class="form-control"
-                                                    placeholder="Default textarea"></textarea>
-                                            </div>
-                                        </div>
-                                    </form>
+                                    </div>
                                 </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="card bg-c-green update-card">
+                                        <div class="card-block">
+                                            <div class="row align-items-end">
+                                                <div class="col-8">
+                                                    <h4 class="text-white">290+</h4>
+                                                    <h6 class="text-white m-b-0">All Tasks</h6>
+                                                </div>
+                                                <div class="col-4 text-right"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; inset: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
+                                                    <canvas id="update-chart-2" height="62" width="110" style="display: block; height: 50px; width: 88px;"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p class="text-white m-b-0"><i class="feather icon-clock text-white f-14 m-r-10"></i>update : 2:15 am</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="card bg-c-pink update-card">
+                                        <div class="card-block">
+                                            <div class="row align-items-end">
+                                                <div class="col-8">
+                                                    <h4 class="text-white">145</h4>
+                                                    <h6 class="text-white m-b-0">Your Tasks</h6>
+                                                </div>
+                                                <div class="col-4 text-right"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; inset: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
+                                                    <canvas id="update-chart-3" height="62" width="110" style="display: block; height: 50px; width: 88px;"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p class="text-white m-b-0"><i class="feather icon-clock text-white f-14 m-r-10"></i>update : 2:15 am</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="card bg-c-lite-green update-card">
+                                        <div class="card-block">
+                                            <div class="row align-items-end">
+                                                <div class="col-8">
+                                                    <h4 class="text-white">500</h4>
+                                                    <h6 class="text-white m-b-0">Compeleted Tasks</h6>
+                                                </div>
+                                                <div class="col-4 text-right"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; inset: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
+                                                    <canvas id="update-chart-4" height="62" width="110" style="display: block; height: 50px; width: 88px;"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p class="text-white m-b-0"><i class="feather icon-clock text-white f-14 m-r-10"></i>update : 2:15 am</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             </div>
 
                         </div>
